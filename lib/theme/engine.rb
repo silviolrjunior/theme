b212436f5a5529d04fbb84acd14594b27406da19
Silvio Ribeiro <silviolrjunior@gmail.com>
2016-08-11 11:17:36 -0300

    fix load concepts

--- lib/theme/engine.rb
+++ lib/theme/engine.rb
@@ -1,5 +1,9 @@
+require 'cells'
+
 module Theme
   class Engine < ::Rails::Engine
     isolate_namespace Theme
+
+    Cell::Concept.view_paths << File.expand_path("#{Theme::Engine.root}/app/concepts", __FILE__)
   end
 end
